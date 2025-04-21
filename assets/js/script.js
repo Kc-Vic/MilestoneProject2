f109db9b159a7ae37606e3ccea1ae15ac29ee210
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-04-21 14:59:37 +0100

    Add event listener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("Game started!");
+            } else {
+                let gameType = this.getAttribute("id");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+});
+
 function startGame() {
 
 }
