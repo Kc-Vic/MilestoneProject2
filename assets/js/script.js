f54dd8565df559fe6114f777f23366936a4f5d47
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-04-21 14:18:43 +0100

    Create structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function startGame() {
+
+}
+
+function generateQuestion() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function displayImage() {
+
+}
+
+function incrementCorrectAnswers() {
+
+}
+
+function incrementWrongAnswers() {
+
+}
